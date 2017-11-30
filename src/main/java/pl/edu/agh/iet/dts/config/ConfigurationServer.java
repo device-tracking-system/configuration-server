68f0f3744f72127c6d2dd23f8849e3253d79d42d
bgrochal <bartek.grochal@gmail.com>
2017-11-30 23:27:38 +0100

    Common configuration for all microservices added. Simple server instance works.

--- src/main/java/pl/edu/agh/iet/dts/config/ConfigurationServer.java
+++ src/main/java/pl/edu/agh/iet/dts/config/ConfigurationServer.java
@@ -1,12 +1,18 @@
 package pl.edu.agh.iet.dts.config;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
 /**
  * @author Bartłomiej Grochal
  */
+@SpringBootApplication
+@EnableConfigServer
 public class ConfigurationServer {
 
     public static void main(String[] args) {
-
+        SpringApplication.run(ConfigurationServer.class, args);
     }
 
 }
